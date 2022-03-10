6ee1e835c898093b0117c4ddd5f0875a95c95081
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-03-10 19:54:04 +0700

    done ch21

--- routes/index.js
+++ routes/index.js
@@ -1,106 +1,145 @@
 var express = require('express');
+const moment = require('moment');
 var router = express.Router();
 var path = require('path');
+
+
 
 module.exports = function (db) {
 
 
-router.get('/',function (req, res,) {
+  router.get('/', function (req, res,) {
 
-  const url = req.url == "/" ? "/?page=1" : req.url
+    const url = req.url == "/" ? "/?page=1&sortBy=id&sortMode=asc" : req.url
 
-  const params = []
-
-
-  if (req.query.id) {
-    params.push(`id like '%${req.query.id}%'`)
-  }
-  if (req.query.boolean) {
-    params.push(`boolean = ${req.query.boolean}`)
-  }
+    const params = []
 
 
-  const page = req.query.page || 1
-  const limit = 3
-  const offset = (page - 1) * limit
-  let sql = `select count(*) as total from todo`;
-  if (params.length > 0) {
-    sql += ` where ${params.join('and')}`
-  }
+    if (req.query.id) {
+      params.push(`id = ${req.query.id}`)
+    }
+    if (req.query.string) {
+      params.push(`stringdata like '%${req.query.string}%'`)
+    }
+    if (req.query.integer) {
+      params.push(`integerdata = ${req.query.integer}`)
+    }
+    if (req.query.float) {
+      params.push(`floatdata = ${req.query.float}`)
+    }
+    if (req.query.startdate && req.query.enddate) {
+      params.push(`datedata between '${req.query.startdate}' and '${req.query.enddate}'`)
+    }
+    if (req.query.boolean) {
+      params.push(`booleandata = ${req.query.boolean}`)
+    }
 
-  db.query(sql, (err, row) => {
-    const pages = Math.ceil(row.total / limit)
-    sql = "select * from todo"
+
+    const page = req.query.page || 1
+    const limit = 3
+    const offset = (page - 1) * limit
+    let sql = `select count(*) as total from todo`;
     if (params.length > 0) {
       sql += ` where ${params.join(' and ')}`
     }
-
-    sql += ` limit $1 offset $2 `
-    db.query(sql, [limit, offset], (err, rows) => {
-      if (err) return res.send(err)
-      res.render('list', { data: rows.rows, page, pages, query: req.query, url});
-    })
-  })  
-})
-
-router.get('/add', function (req, res) {
-  res.render('add')
-})
-
-router.post('/add', function (req, res) {
-  console.log(req.body)
-  let string = req.body.string
-  let integer = parseInt(req.body.integer)
-  let float = parseFloat(req.body.float)
-  let date = req.body.date
-  let boolean = JSON.parse(req.body.boolean)
-  
-
-
-  //Quary Binding
-  db.query('insert into todo(stringdata, integerdata, floatdata, datedata, booleandata) values ($1, $2, $3, $4, $5)', [string, integer, float, date, boolean], (err) => {
-    if (err){
+    console.log(sql)
+    db.query(sql, (err, row) => {
       console.log(err)
-      return res.send(err)
-    } 
-    res.redirect('/')
-  })
-})
+      const pages = Math.ceil(row.rows[0].total / limit)
+      sql = "select * from todo"
+      if (params.length > 0) {
+        sql += ` where ${params.join(' and ')}`
+      }
+      req.query.sortMode = req.query.sortMode || 'asc';
 
-router.get('/delete/:id', function (req, res) {
-  const id = req.params.id
-  db.query('delete from todo where id = $1', [Number(id)], (err) => {
-    if (err) return res.send(err)
-    res.redirect('/')
-  })
-})
+      req.query.sortBy = req.query.sortBy || 'id';
 
-router.get('/edit/:id', function (req, res) {
-  const id = req.params.id
-  db.query('select * from todo where id = $1', [Number(id)], (err, item) => {
-    if (err) return res.send(err)
-    res.render('edit', { data: item })
-  })
-})
+      sql += ` order by ${req.query.sortBy} ${req.query.sortMode}`
 
-router.post('/edit/:id', function (req, res) {
-  const id = Number(req.params.id)
-  const string = req.body.string
-  const integer = req.body.integer
-  const float = req.body.float
-  const date = req.body.date
-  const boolean = req.body.boolean
-  console.log(req)
-  
-  db.query('update todo set string = $1, integer = $2, float = $3, date = $4, boolean = $5 where id = $6', [string, integer, float, date, boolean, id], (err, row) => {
-    if (err) return res.send(err)
-    
-    res.redirect('/')
+      sql += ` limit $1 offset $2 `
+      db.query(sql, [limit, offset], (err, rows) => {
+        if (err) return res.send(err)
+        db.query('select * from todo order by id', (err, todo) => {
+          if (err) return res.send(err)
+          db.query('select * from todo order by stringdata', (err, todo) => {
+            if (err) return res.send(err)
+            res.render('list', {
+              data: rows.rows,
+              page,
+              pages,
+              query: req.query,
+              url,
+              todo: todo.rows,
+              moment
+            });
+
+          })
+        })
+      })
+    })
+  })
+
+  router.get('/add', function (req, res) {
+    res.render('add')
+  })
+
+  router.post('/add', function (req, res) {
+    console.log(req.body)
+    let string = req.body.string
+    let integer = parseInt(req.body.integer)
+    let float = parseFloat(req.body.float)
+    let date = req.body.date
+    let boolean = JSON.parse(req.body.boolean)
+
+
+
+    //Quary Binding
+    db.query('insert into todo(stringdata, integerdata, floatdata, datedata, booleandata) values ($1, $2, $3, $4, $5)', [string, integer, float, date, boolean], (err) => {
+      if (err) {
+        console.log(err)
+        return res.send(err)
+      }
+      res.redirect('/')
+    })
+  })
+
+  router.get('/delete/:id', function (req, res) {
+    const id = req.params.id
+    db.query('delete from todo where id = $1', [Number(id)], (err) => {
+      if (err) return res.send(err)
+      res.redirect('/')
+    })
+  })
+
+  router.get('/edit/:id', function (req, res) {
+    db.query('select * from todo where id = $1', [Number(req.params.id)], (err, item) => {
+      if (err) return res.send(err)
+
+      res.render('edit', {
+        data: item.rows[0],
+        moment
+      })
+    })
+  })
+
+  router.post('/edit/:id', function (req, res) {
+    const id = Number(req.params.id)
+    const string = req.body.string
+    const integer = req.body.integer
+    const float = req.body.float
+    const date = req.body.date
+    const boolean = req.body.boolean
+    console.log(req)
+
+    db.query('update todo set stringdata = $1, integerdata = $2, floatdata = $3, datedata = $4, booleandata = $5 where id = $6', [string, integer, float, date, boolean, id], (err, row) => {
+      if (err) return res.send(err)
+
+      res.redirect('/')
+    });
   });
-  });
 
 
 
 
-return router;
+  return router;
 }
